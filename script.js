2952bdc3f10d21f8d63c53282bd53cbf151f8710
leejblack29 <leejblack29@gmail.com>
2024-02-01 15:18:06 -0500

    Make createDiv return the newly created div
    When calling this function later on, it will allow for easier
    accessibility

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createDiv(parent) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("flex", "even", "column");
     parent.appendChild(newDiv);
+    return newDiv;
 }
 
 createDiv(grid);
