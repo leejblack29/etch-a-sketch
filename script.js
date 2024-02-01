41fd1b48818a37108d634a5c953c7c0c291b3ed9
leejblack29 <leejblack29@gmail.com>
2024-02-01 14:27:18 -0500

    Define createColumn function to add new column

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const grid = document.querySelector("#grid");
 
+function createColumn() {
+    const newCol = document.createElement("div");
+    newCol.classList.add("flex", "even", "column");
+    grid.appendChild(newCol);
+}
+
+createColumn();
+createColumn();
+createColumn();
+createColumn();
+createColumn();
+createColumn();
