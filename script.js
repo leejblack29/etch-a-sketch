aeb8d7eeda09dcb2dce697bb7334c6920ef10134
leejblack29 <leejblack29@gmail.com>
2024-02-01 16:31:08 -0500

    Write deleteGrid function, call on button click

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 const grid = document.querySelector("#grid");
 
 function createDiv(parent) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("flex", "even", "column");
-    parent.appendChild(newDiv);
-    return newDiv;
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("flex", "even", "column");
+  parent.appendChild(newDiv);
+  return newDiv;
 }
 
 function generateGrid(n) {
-    // create n columns
-    for (let i = 0; i < n; i++) {
-        const thisColumn = createDiv(grid)
+  // create n columns
+  for (let i = 0; i < n; i++) {
+    const thisColumn = createDiv(grid);
 
-        // populate each column with n divs
-        for (let j = 0; j < n; j++) {
-            createDiv(thisColumn).classList.add("box");
-        }
+    // populate each column with n divs
+    for (let j = 0; j < n; j++) {
+      createDiv(thisColumn).classList.add("box");
     }
+  }
 }
 
 function makeDrawable() {
-    grid.addEventListener("mouseover", (event) => {
-        const thisDiv = event.target;
+  grid.addEventListener("mouseover", (event) => {
+    const thisDiv = event.target;
 
-        if (thisDiv.classList.contains("box")) {
-            thisDiv.style.backgroundColor = "black";
-        }   
-    } );
-
+    if (thisDiv.classList.contains("box")) {
+      thisDiv.style.backgroundColor = "black";
+    }
+  });
 }
 
-generateGrid(100);
+function deleteGrid() {
+  const divs = grid.querySelectorAll("#grid > div");
+  divs.forEach((div) => {
+    div.remove();
+  });
+}
+
+function setRes() {
+  const button = document.querySelector("button");
+  button.addEventListener("click", deleteGrid);
+}
+
+generateGrid(16);
 makeDrawable();
-
-
-
-
-
-
+setRes();
